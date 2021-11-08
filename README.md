9134ffec66ee68c17f98a2e426755bf335057e6a
Burak AKIN <akinburak94@gmail.com>
2021-11-08 21:20:25 +0300

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # AutoMarket
+
+## Ön Koşullar
+
+* .NET 5 SDK
+* Postman
+
+## Kullanım
+
+``dotnet build`` komutu ile projeyi derleyiniz.
+
+src\AutoMarket.Api dizini içerisinde ``dotnet run`` komutu ile projeyi çalıştırabilirsiniz.
+
+``dotnet test`` komutu ile unit testleri çalıştırabilirsiniz.
---
+++ src/AutoMarket.Api/Startup.cs
@@ -0,0 +1,57 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoMarket.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = ApiVersion.Default;
+            });
+            services.AddMediatR(typeof(Startup));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
