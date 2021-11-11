e1151c7ac44992db229721ed88795dc82d406253
Burak AKIN <akinburak94@gmail.com>
2021-11-11 19:23:07 +0300

    DbContext added

---
+++ src/AutoMarket.Api/Infrastructures/Database/AutoMarketDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AutoMarket.Api.Infrastructures.Database
+{
+    public class AutoMarketDbContext : DbContext
+    {
+        public AutoMarketDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
